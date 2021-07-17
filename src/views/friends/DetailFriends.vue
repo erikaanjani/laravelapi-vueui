fb33ce53e7c6b7c89c2b1292de9c6110502e704c
erikaanjani <erikaanjanii17@gmail.com>
2021-07-16 19:37:13 -1200

    commit pert 11

--- src/views/friends/DetailFriends.vue
+++ src/views/friends/DetailFriends.vue
@@ -12,46 +12,96 @@
         {{friend.alamat}}
         </p>
   </div>
+  <div class="card text-center mt-4" v-for="f in friend" :key ="f.id">
+  <div class="card-header">Detail Teman</div>
+  <div class="card-body">
+    <h5 class="card-title">{{f.nama}}</h5>
+    <p class="card-text">{{f.no_tlp}}</p>
+    <p class="card-text">{{f.alamat}}</p>
+    <p class="card-text">{{f.groups.name}}</p>
+  </div>
   <div class="card-footer">
-    <router-link
-     class="btn btn-success" 
-    :to="{name:'Editfriends', params: {id:friend.id}}"
-          >EDIT</router-link
-          >
-        <button @click.prevent="friendDelete(friend.id)" class="btn 
-        btn-danger">DELETE</button>
+   <router-link
+              class="btn btn-success"
+              :to="{ name: 'Editfriends', params: { id: f.id } }"
+              >Edit</router-link
+            >
+            <button
+              @click.prevent="friendDelete(f.id)"
+              class="btn btn-danger"
+            >
+              Delete
+            </button>
   </div>
 </div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router'
+import axios from 'axios'
 import { onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
 export default {
-  setup() {
-      let friend = ref([]);
+setup() {
+let friend = ref([]);
+const router = useRouter();
+const route = useRoute()
+onMounted(()=>{
+axios.get(`http://127.0.0.1:8000/api/friends/${route.params.id}`)
+.then(response => {
+console.log(response.data.data.nama);
+friend.value = response.data.data;
+}).catch(error =>{
+console.log(error.response.data);
+});
+});
+function friendDelete(id){
+axios.delete(`http://127.0.0.1:8000/api/friends/${id}`)
+.then(()=>{
+router.go(-1);
+}).catch(error => {
+console.log(error)
+})
+}
+return {
+friend,
+router, 
+friendDelete,
+route
+}
+},
+}
+</script> 
+  setup() { 
+    
+    let friend =ref([]);
     const router = useRouter();
     const route = useRoute();
-    onMounted(()=>(
-      axios.get(`http://127.0.0.1:8000/api/friends/${route.params.id}`)
-      .then(response => {
-        console.log(response.data.data.nama);
-        friend.value = response.data.data;
-      })
-      .catch(error => {
-        console.log(error.response.data);
-      })
-    ));
-    function friendDelete(id){
-      axios.delete(`http://127.0.0.1:8000/api/friends/${id}`)
-      .then(()=>{
+    onMounted(() => {
+      axios
+        .get(` http://127.0.0.1:8000/api/friends/${route.params.id}`)
+        .then((Response) => {
+          console.log(Response.data.data.nama);
+          friend.value = Response.data.data;
+         
+        })
+        .catch((error) => {
+          console.log(error.Response.data);
+        });
+    });
+   function friendDelete(id){
+      axios
+        .delete(`http://127.0.0.1:8000/api/friends/${id}`)
+        .then(() => {
           router.go(-1);
-      }).catch(error => {
-        console.log(error)
-      })
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-   
+    
     return {
       friend,
       router,
@@ -63,4 +113,4 @@ export default {
 </script>
 
 <style>
-</style> 
+</style>
